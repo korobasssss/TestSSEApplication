ab78efb7978789dd2d21c0bb78ff596078939e10
korobasssss <an.korobeynikova@gmail.com>
2024-05-06 00:46:26 +0300

    new commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,32 @@
 
 import styles from "./page.module.css";
 import {useEffect, useState} from "react";
+import axios from "axios";
 
 export default function Home() {
     const [data, setData] = useState([] as any)
+    const [isClicked, setIsClicked] = useState(false)
+    const [input_message, setInput_message] = useState('')
 
     useEffect(() => {
-        const event = new EventSource('http://localhost:8080/stream')
+        if (isClicked) {
+            axios.post('http://localhost:8080/test',
+                {
+                    data: input_message
+                })
+                .then((response => {
+                    if (response.status === 200) console.log('ok')
+                    else console.log('error')
+                }))
+                .catch(error => {
+                    console.log(error)
+                })
+        }
+    }, []);
+
+
+    useEffect(() => {
+        const event = new EventSource('http://localhost:8080/something')
 
         event.onmessage = (event) => {
             setData(event.data);
@@ -16,17 +36,20 @@ export default function Home() {
         return () => {
             event.close();
         };
-    }, []);
+    }, [isClicked]);
 
     return (
         <main className={styles.main}>
             <section className={styles.section}>
-                <div>
-                    Смотри на меня и больше никакого тп не будет в твоей жизни
-                </div>
+                <input placeholder={'Эх'}
+                       value={input_message}
+                       onChange={(event) => setInput_message(event.target.value)}/>
+                <button onClick={() => setIsClicked(true)}>
+                    Нажми на меня и больше никакого тп не будет в твоей жизни
+                </button>
             </section>
             <ul className={styles.ul}>
-                {data.map((text : string, index: number) => {
+                {data.map((text: string, index: number) => {
                     return (
                         <li key={index}>{text}</li>
                     )
